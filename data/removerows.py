2d4c078540598f0804b09f135f6fe1e4fb9b1d82
Matthew Malone <matthewmalon@umass.edu>
2019-03-08 21:19:56 -0500

    Baseline experimenting, need to cleanup

---
+++ data/removerows.py
@@ -0,0 +1,34 @@
+import csv
+import sys
+import enchant
+# simple script to delete (kinda) delete non english rows
+# and also delete movies with no genre
+#python 2.7
+filename = sys.argv[1]
+d = enchant.Dict("en_US")
+
+# with open(filename, 'rb') as inp, open('first_edit.csv', 'wb') as out:
+#     writer = csv.writer(out)
+#     for row in csv.reader(inp):
+#       canWrite = True
+#       for word in str(row).split()
+#         if d.check(word) is False:
+#             canWrite = False
+#       if canWrite is True:
+#         writer.writerow(row)
+
+with open(filename, "rt", encoding='utf-8') as f:
+    data = list(csv.reader(f))
+
+with open("new.csv", "wt") as f:
+    writer = csv.writer(f)
+    for row in data:
+      canWrite = True
+      strRow = str(row[0])
+      for word in strRow.split():
+        if not word:
+          pass
+        elif d.check(word) is False:
+            canWrite = False
+      if canWrite is True:
+        writer.writerow(row)
