8d4143565b1d853c0184b140eab84052b97c88a3
Matthew Malone <matthewmalon@umass.edu>
2019-03-18 15:29:08 -0400

    -Data improvements (much more to be done) -Baseline model trained, will try conv1d later -Generalized row removal script for imdb data downloaded online

--- data/removerows.py
+++ data/removerows.py
@@ -1,24 +1,37 @@
 import csv
 import sys
 import enchant
-# simple script to delete (kinda) delete non english rows
-# and also delete movies with no genre
-#python 2.7
+
+# Removes from IMDB database:
+# -Non english movies
+# -Movies with no genre
+# -Adult Movies
+# -TV shows
+# -Maybe remove certain genres
+
 filename = sys.argv[1]
 d = enchant.Dict("en_US")
 
-with open(filename, "rt", encoding='utf-8') as f:
-    data = list(csv.reader(f))
-
-with open("new.csv", "wt") as f:
-    writer = csv.writer(f)
-    for row in data:
-      canWrite = True
-      strRow = str(row[0])
-      for word in strRow.split():
-        if not word:
-          pass
-        elif d.check(word) is False:
-            canWrite = False
-      if canWrite is True:
-        writer.writerow(row)
+with open(filename, "rt", encoding='ISO-8859-1') as t:
+  data = csv.reader(t, delimiter='\t')
+  with open("new.csv", "wt") as f:
+      writer = csv.writer(f, delimiter='\t')
+      for row in data:
+        canWrite = True
+        if row[8] == '\\N':
+          canWrite = False
+        strRow = str(row[2])
+        for word in strRow.split():
+          if not word:
+            pass
+          elif d.check(word) is False:
+              canWrite = False
+              break
+        if str(row[1]) != 'movie':
+          canWrite = False
+        if row[4] == 1:
+          canWrite = False
+        if canWrite is True:
+          #print(row[0])
+          #writer.writerow([row[2], row[8]])
+          writer.writerow([row[0], row[1]])
