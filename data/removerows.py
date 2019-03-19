15c3a6f10190e75540f472b2d5314fab831090db
Matthew Malone <matthewmalon@umass.edu>
2019-03-19 00:11:39 -0400

    -Data cleanup mostly done -Baseline model trained(Okay accuracy but actual predictions are boring and seem to get it wrong from a human perspective) -Conv1D model needs to be trained past 2 epoch but my battery is about to explode

--- data/removerows.py
+++ data/removerows.py
@@ -1,37 +1,29 @@
 import csv
 import sys
-import enchant
 
 # Removes from IMDB database:
-# -Non english movies
 # -Movies with no genre
 # -Adult Movies
-# -TV shows
-# -Maybe remove certain genres
+# -Non Movies
 
 filename = sys.argv[1]
-d = enchant.Dict("en_US")
 
 with open(filename, "rt", encoding='ISO-8859-1') as t:
   data = csv.reader(t, delimiter='\t')
-  with open("new.csv", "wt") as f:
+  with open("new.tsv", "wt") as f:
       writer = csv.writer(f, delimiter='\t')
       for row in data:
+        #print(row[0])
         canWrite = True
-        if row[8] == '\\N':
-          canWrite = False
-        strRow = str(row[2])
-        for word in strRow.split():
-          if not word:
-            pass
-          elif d.check(word) is False:
-              canWrite = False
-              break
+        try:
+          if row[8] == '\\N':
+            canWrite = False
+        except:
+          pass
         if str(row[1]) != 'movie':
           canWrite = False
         if row[4] == 1:
           canWrite = False
-        if canWrite is True:
-          #print(row[0])
-          #writer.writerow([row[2], row[8]])
-          writer.writerow([row[0], row[1]])
+        if canWrite == True:
+          print(row[0])
+          writer.writerow([row[2], row[8]])
